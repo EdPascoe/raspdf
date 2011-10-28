5e077f2ac90f5de0b5a530e975d29be0216d6635
Rascal Angola machine <ed@tsf.co.za>
2011-10-28 09:28:05 +0100

    provided page size to match old incorrect xxpdf A4 page size

--- lib/RascalPDF.py
+++ lib/RascalPDF.py
@@ -43,9 +43,9 @@ class RascalPDF:
   canvas = None
   font_size = 10 #Default font size
   #Margins
-  lmargin = 1.5*cm
-  tmargin = 1.5*cm
-  bmargin = 1.5*cm
+  lmargin = 0.5*cm
+  tmargin = 0.5*cm
+  bmargin = 1*cm
   lmarginDefault = None
   line="NOT YET INITITALIZED"
   fileLocator = None
@@ -503,9 +503,10 @@ class PrintJob:
   landscape = False
   imagedirs = ["images", "templates"]; #TODO this should be in a config file somewhere.
   
-  def __init__(self, output=None, landscape=False ):
+  def __init__(self, output=None, pagesize = reportlab.lib.pagesizes.A4, landscape=False ):
     """ fhandle should be a file like object. 
     """
+    self.pagesize = pagesize
     self.landscape = landscape
 
     #Try build up a fairly detailed list of paths to search for files. 
@@ -550,7 +551,7 @@ class PrintJob:
       self.pdffile = tempfile.NamedTemporaryFile(suffix='_auto.pdf' ) #Temporary file with the work auto in it to force auto starting in terraterm.
     
     self._1stParse(fhandle)
-    self.rascalpdf = RascalPDF(self.pdffile, pagesize=reportlab.lib.pagesizes.A4, isLandscape=self.landscape, fileLocator=self.fileLocator)
+    self.rascalpdf = RascalPDF(self.pdffile, pagesize=self.pagesize, isLandscape=self.landscape, fileLocator=self.fileLocator)
 
     self.rascalpdf.info.producer = "xxpdf2 by Ed.Pascoe <ed@pascoe.co.za>"
     self.rascalpdf.info.tile="Rascal document"
@@ -567,6 +568,11 @@ class PrintJob:
     self.parser = _Parser(fileLocator=self.fileLocator)
     self.parser.addCommand('PRINTINIT')
     for line in fhandle:
+      # -------- Strip any control characters -----------
+      line = line.replace(chr(0x0f),'') # ^O
+      line = line.replace(chr(0x1b),'') # ^[ Escape
+      line = line.replace(chr(0x12),'') # ^R
+      # convert the line into function calls for 2nd parse later.
       self.parser.parseLine(line)
     self.parser.addCommand('PRINTEND')
     lineno = 0
--- lib/raspdf.py
+++ lib/raspdf.py
@@ -7,6 +7,7 @@ Replacement for the old xxpdf program.
 import logging
 import optparse
 import os, sys, time, tempfile
+import reportlab.lib.pagesizes
 from subprocess import *
 
 import RascalPDF
@@ -22,7 +23,7 @@ def main():
   parser.add_option("--debug", dest="debug", action="store_true", help="Show debugging information")
   parser.add_option("-f", "--outputfile", dest="outputfile", type="string", help="Send output to file with given name instead of a temp file.")
   parser.add_option("--tty", dest="tty", type="string", help="The running TTY to conenct to for zmodem")
-  parser.add_option("-r", "--reset", dest="reset", action="store_true", help="Reset the terminal after running.")
+  parser.add_option("-x", "--xxpdf", dest="xxpdf", action="store_true", help='Use xxpdf defaults including the broken A4 page size of 8.19" x 12.36" instead of 8.27" x 11.69" ')
 
   (options, args) = parser.parse_args()
 
@@ -43,7 +44,11 @@ def main():
     tf = tempfile.NamedTemporaryFile(suffix='_auto.pdf' ) #Temporary file with the work auto in it to force auto starting in terraterm.
     outfile = tf.name
     log.debug("Outfile: %s", outfile)
-  c = RascalPDF.PrintJob(output=outfile, landscape=options.landscape )
+
+  if options.xxpdf: pagesize = (590, 890) # Use the old incorect page sizes from xxpdf.
+  else: pagesize = reportlab.lib.pagesizes.A4
+
+  c = RascalPDF.PrintJob(output=outfile, pagesize=pagesize, landscape=options.landscape)
     
   if args: c.feed(file(args[0]))
   else: c.feed(sys.stdin)
@@ -57,15 +62,12 @@ def main():
 
   if options.zmodem:
     if not options.tty: options.tty=os.environ['TTY']
-    if options.reset: #Complicated hack because sz has a habit of messing up the screen.
-      resetstr = "; sleep 3 ; clear < %s > %s " % (options.tty, options.tty)
-    else:
-      resetstr = " "
+    resetstr = "; sleep 3 ; clear < %s > %s " % (options.tty, options.tty) #Complicated hack because sz has a habit of messing up the screen.
     cmd = "sz -eq %s < %s > %s %s " % (outfile, options.tty, options.tty, resetstr)
     log.debug( cmd)
     os.system(cmd)
     if options.reset:
       for cmd in [ "reset", "tput rmacs", "tput krfr", "clear", "echo ' '"   ] :
-	 cmdstr = cmd + " < %s > %s " % (options.tty, options.tty)
-	 os.system(cmdstr)
+        cmdstr = cmd + " < %s > %s " % (options.tty, options.tty)
+        os.system(cmdstr)
 
