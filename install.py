11611844232d17b8e81a078d5b2291ab13cf4b0f
Ed Pascoe <ed@tsf.co.za>
2012-02-13 09:47:12 +0200

    --never-download had been removed from virtualenv which was causing issues

--- install.py
+++ install.py
@@ -14,6 +14,16 @@ __status__ = "Production"
 env_location="ENV" #Python environment. If does not start with / will be relative to location of install.py
 
 import os, os.path, sys
+import logging
+
+log = logging.getLogger("root")
+formatter = logging.Formatter("%(levelname)s %(module)s:%(lineno)d: %(message)s")
+output = logging.StreamHandler()
+output.setFormatter(formatter)
+log.addHandler(output)
+log.setLevel(logging.info)
+
+log = logging.getLogger("root")
 
 root = os.path.dirname(os.path.abspath(__file__))
 os.chdir(root)
@@ -25,17 +35,20 @@ else:
   destenv = os.path.join(root,env_location)
 
 #Make sure the virtual environment is running.
+log.debug("Testing for existance of '%s'", destenv)
 if not os.path.exists(destenv):
   import virtualenv
-  oldargv = sys.argv  #Save command line options
-  sys.argv = [sys.argv[0], '--never-download', '--distribute', destenv] #We are going to let virtualenv think it is running on its own.
+  oldargv = sys.argv  #Save command line options 
+  # '--never-download has been removed.
+  sys.argv = [sys.argv[0], '--distribute', destenv] #We are going to let virtualenv think it is running on its own.
+  log.debug("Calling virtualenv.main (%s)", sys.argv)
   virtualenv.main()
   sys.argv = oldargv #Restore command line options.
 
 pip = os.path.join(destenv, "bin/pip")
 
 cmd= "%s install -v --no-index -r install/requirements.txt --environment=%s" % (pip,  destenv)
-print cmd
+log.debug("executing %s", cmd)
 os.system(cmd)
 if not os.path.exists("raspdf"):
   f=file("raspdf","w")
